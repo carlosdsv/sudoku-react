cb55f44d2bd1a18aff5057d7e9bce21a8e9b6a84
carlosdsv <carlosdsv@gmail.com>
2021-03-25 16:34:58 +0100

    refactor import paths

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { ThemeProvider } from 'styled-components'
 
-import { Card, Content, Grid, Title } from './components'
-import { unregister } from './core'
-import { GlobalStyles, theme } from './styles'
-import reportWebVitals from './reportWebVitals'
+import { Card, Content, Grid, Title } from 'components'
+import { unregister } from 'core'
+import { GlobalStyles, theme } from 'styles'
+import reportWebVitals from 'reportWebVitals'
 
 ReactDOM.render(
   <React.StrictMode>
