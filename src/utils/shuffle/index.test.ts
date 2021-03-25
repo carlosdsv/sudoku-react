daea0b6d7d956c4e196cb193272136c3f6d220e5
carlosdsv <carlosdsv@gmail.com>
2021-03-25 17:36:20 +0100

    remove comments in test

--- src/utils/shuffle/index.test.ts
+++ src/utils/shuffle/index.test.ts
@@ -2,13 +2,11 @@ import shuffle from './'
 
 describe('shuffle', () => {
   it('returns an array with the same length after being shuffled', () => {
-    //wip
     const array = [1, 2, 3]
     shuffle(array)
     expect(array).toHaveLength(3)
   })
   it('returns an array with the same elements after being shuffled', () => {
-    //wip
     const array = [1, 2, 3]
     shuffle(array)
     expect(array).toContain(1)
